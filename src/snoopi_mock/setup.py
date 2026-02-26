faeabf730ac455d7fbf09221e9f28c9bd4bf14e2
Juan Reyes <reyesj20703@gmail.com>
2026-02-26 00:04:51 -0500

    fix: add ament index marker and package.xml install to snoopi_mock
    Resolves colcon warnings about missing package index marker and
    implicit package.xml installation.

--- src/snoopi_mock/setup.py
+++ src/snoopi_mock/setup.py
@@ -1,9 +1,16 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 setup(
     name='snoopi_mock',
     version='0.1.0',
     packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/snoopi_mock']),
+        ('share/snoopi_mock', ['package.xml']),
+    ],
     install_requires=['setuptools'],
     entry_points={
         'console_scripts': [
